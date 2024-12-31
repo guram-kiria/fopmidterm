92e881cbf558bc0138ec94fc60d5be90e34c45bf
copperb <kiriaguram420@gmail.com>
2024-12-31 19:26:05 +0400

    fixed issues in previous commit, if and else left

--- src/parser/Parser.java
+++ src/parser/Parser.java
@@ -12,11 +12,9 @@ public class Parser {
 
     private static final Map<String, Integer> variables = new HashMap<>();
     private static int tokenIterator = 0;
-    private static int iteratorStore = tokenIterator;
+
 
     public static void execute(List<Token> code) {
-
-        iteratorStore = tokenIterator;
         tokenIterator = 0;
 
         while (tokenIterator < code.size()) {
@@ -24,9 +22,10 @@ public class Parser {
             if (token.getType().equals(TokenType.PRINT)) {
                 // Handle print statement
                 if (tokenIterator + 1 < code.size() && code.get(tokenIterator + 1).getType().equals(TokenType.PARENTHESIS) && code.get(tokenIterator + 1).getValue().equals("(")) {
-                    int endIndex = findMatchingParenthesis(code, tokenIterator + 1);
+                    int endIndex = tokenIterator + 3;
                     List<Token> expression = code.subList(tokenIterator + 2, endIndex);
                     String thingToPrint = expression.getLast().getValue();
+
                     if (thingToPrint.matches("\\d+")) {
                         System.out.print(thingToPrint);
                     } else if (thingToPrint.matches("[a-zA-Z0-9]+")) {
@@ -44,51 +43,26 @@ public class Parser {
                     // Handle potential syntax error (missing parentheses)
                     System.err.println("Syntax Error: Expected '(' after 'print'");
                 }
-            } else if (token.getType().equals(TokenType.IDENTIFIER) && code.get(tokenIterator + 1).getType().equals(TokenType.OPERATOR)) {
-                if (code.get(tokenIterator + 1).getValue().equals("=")) {
+            } else if (token.getType().equals(TokenType.IDENTIFIER)) {
+                if (code.get(tokenIterator + 1).getType().equals(TokenType.OPERATOR) && code.get(tokenIterator + 1).getValue().equals("=")) {
                     // find where right side of the assignment operation ends
                     List<Token> rightSide = findRightSideOfAssignment(tokenIterator + 1, code);
 
                     //write in map variable and its value
                     variables.put(token.getValue(), evaluateExpression(rightSide));
-                } else {
-                    tokenIterator++;
                 }
             } else if (token.getType().equals(TokenType.NEWLINE)) {
-                System.out.println("===================NEWLINE==========================");
-               tokenIterator++;
-               continue;
+                tokenIterator++;
             } else if (token.getType().equals(TokenType.WHILE)) {
-                System.out.println("===================WHILE==========================");
                 Map<String, List<Token>> parts = getConditionParts(code);
                 List<Token> block = getBlock(code);
 
                 executeBlock(parts, block);
-            } else if (token.getType().equals(TokenType.INDENT)) {
-                System.out.println("===================INDENT==========================");
-                tokenIterator++;
-            } else if (token.getType().equals(TokenType.DEDENT)) {
-                System.out.println("===================DEDENT==========================");
-                tokenIterator ++;
-                continue;
-            }
-
-            else if (token.getType().equals(TokenType.NUMBER)) {
-                System.out.println("===================NUMBER==========================");
-                tokenIterator++;
-                continue;
-            } else if (token.getType().equals(TokenType.OPERATOR)){
-                tokenIterator++;
-                continue;
-            }
-
-            else {
+            } else {
                 System.out.println(token);
                 System.out.println("//////////////////////////////////////");
             }
 
-            tokenIterator++;
-            //TODO =========================================+  check if tokenIterator is increased normally +=========================================
         }
 
     }
@@ -104,18 +78,24 @@ public class Parser {
         List<Token> condition = code.subList(tokenIterator + 1, iter);
 
         int expIter = 0;
-        while (expIter < condition.size() && !condition.get(expIter).getValue().matches("[<>]")){
+        while (expIter < condition.size() && !condition.get(expIter).getValue().matches("([<>=])")) {
             expIter++;
         }
 
         parts.put("leftExpression", new ArrayList<>(condition.subList(0, expIter)));
         List<Token> operator = new ArrayList<>();
         operator.add(condition.get(expIter));
+        if (condition.get(expIter + 1).getType().equals(TokenType.OPERATOR)) {
+            operator.add(condition.get(expIter + 1));
+            expIter++;
+        }
+
+
         parts.put("operator", operator);
 
         int operatorIndex = expIter;
 
-        while (expIter < condition.size() && !condition.get(expIter).getValue().equals(":")){
+        while (expIter < condition.size() && !condition.get(expIter).getValue().equals(":")) {
             expIter++;
         }
         tokenIterator += expIter + 1;
@@ -124,36 +104,51 @@ public class Parser {
         return parts;
     }
 
-
     private static void executeBlock(Map<String, List<Token>> condition, List<Token> block) {
         List<Token> left = condition.get("leftExpression");
 
         List<Token> right = condition.get("rightExpression");
-        Token operator = condition.get("operator").getFirst();
+        List<Token> operator = condition.get("operator");
 
+        boolean currValue = checkCondition(evaluateExpression(left), evaluateExpression(right), operator);
 
-        if (operator.getType().equals(TokenType.OPERATOR)) {
-            System.out.println("===================BLOCK OPERATOR==========================");;
-            boolean currValue = checkCondition(evaluateExpression(left), evaluateExpression(right), operator);
-
-            while (currValue) {
-                execute(block);
-                currValue = checkCondition(evaluateExpression(left), evaluateExpression(right), operator);
-            }
-            tokenIterator = iteratorStore + block.size() + 1;
+        int iteratorStore = tokenIterator;
+        while (currValue) {
+            execute(block);
+            currValue = checkCondition(evaluateExpression(left), evaluateExpression(right), operator);
         }
+        tokenIterator = iteratorStore;
 
     }
 
-    private static boolean checkCondition(int firstNum, int secondNum, Token operator) {
-        switch (operator.getValue()) {
+    private static boolean checkCondition(int firstNum, int secondNum, List<Token> operator) {
+        StringBuilder oper = new StringBuilder();
+        for (Token token : operator) {
+            if (token.getType().equals(TokenType.OPERATOR)) {
+                oper.append(token.getValue());
+            }
+        }
+        String fullOperator = oper.toString();
+        switch (fullOperator) {
             case "<" -> {
                 return firstNum < secondNum;
             }
             case ">" -> {
                 return firstNum > secondNum;
             }
-            default -> throw new RuntimeException("Invalid operator for while loop");
+            case ">=" -> {
+                return firstNum >= secondNum;
+            }
+            case "<=" -> {
+                return firstNum <= secondNum;
+            }
+            case "==" -> {
+                return firstNum == secondNum;
+            }
+            case "!=" -> {
+                return firstNum != secondNum;
+            }
+            default -> throw new RuntimeException("Invalid operator for boolean condition");
 
         }
     }
@@ -178,7 +173,6 @@ public class Parser {
         return block;
     }
 
-
     private static final Stack<Token> expressionTokens = new Stack<>();
 
     private static Integer evaluateExpression(List<Token> expression) {
@@ -191,13 +185,17 @@ public class Parser {
             Token firstToken = expressionCopy.get(0);
             if (firstToken.getType().equals(TokenType.NUMBER)) {
                 return Integer.parseInt(firstToken.getValue());
+            } else if (firstToken.getType().equals(TokenType.IDENTIFIER)) {
+                return variables.get(firstToken.getValue());
             }
         }
 
-        for (Token token : expressionCopy) {
-            if (token.getType().equals(TokenType.OPERATOR) && !token.getValue().equals("=") || token.getType().equals(TokenType.NUMBER) || token.getType().equals(TokenType.IDENTIFIER)) {
-                if (expressionTokens.isEmpty() && ((token.getType().equals(TokenType.IDENTIFIER) || token.getType().equals(TokenType.NUMBER)))) {
 
+        for (Token token : expressionCopy) {
+
+            //token.getType().equals(TokenType.OPERATOR) && !token.getValue().equals("=") ||
+            if (token.getType().equals(TokenType.NUMBER) || token.getType().equals(TokenType.IDENTIFIER) || token.getType().equals(TokenType.OPERATOR)) {
+                if (expressionTokens.isEmpty() && ((token.getType().equals(TokenType.IDENTIFIER) || token.getType().equals(TokenType.NUMBER)))) {
                     expressionTokens.push(token);
                 }
                 switch (token.getType()) {
@@ -212,12 +210,12 @@ public class Parser {
                         }
                     }
                     case TokenType.OPERATOR -> {
-                        if (expressionTokens.isEmpty()){
+                        if (expressionTokens.isEmpty() || expressionTokens.peek().getType().equals(TokenType.OPERATOR)) {
                             throw new RuntimeException("Illegal syntax");
-                        }
-                        if (!expressionTokens.peek().getType().equals(TokenType.OPERATOR)) {
+                        } else {
                             expressionTokens.push(token);
                         }
+
                     }
                 }
             } else {
@@ -229,7 +227,17 @@ public class Parser {
             throw new RuntimeException("wrong right side in expression");
         }
 
-        return Integer.parseInt(expressionTokens.pop().getValue());
+        try {
+            int ans = Integer.parseInt(expressionTokens.peek().getValue());
+            expressionTokens.pop();
+            return ans;
+        } catch (NumberFormatException e) {
+            int ans = variables.get(expressionTokens.pop().getValue());
+            expressionTokens.pop();
+            return ans;
+        }
+
+
     }
 
     private static Token calculateCurrentExpression(int currentNumValue) {
@@ -244,11 +252,11 @@ public class Parser {
         int currAns = 0;
 
         switch (operator.getValue()) {
-            case "+" -> currAns = currentNumValue + firstNum;
-            case "-" -> currAns = currentNumValue - firstNum;
-            case "*" -> currAns = currentNumValue * firstNum;
-            case "/" -> currAns = currentNumValue / firstNum;
-            case "%" -> currAns = currentNumValue % firstNum;
+            case "+" -> currAns = firstNum + currentNumValue;
+            case "-" -> currAns = firstNum - currentNumValue;
+            case "*" -> currAns = firstNum * currentNumValue;
+            case "/" -> currAns = firstNum / currentNumValue;
+            case "%" -> currAns = firstNum % currentNumValue;
         }
 
         return new Token(TokenType.NUMBER, String.valueOf(currAns));
@@ -267,19 +275,19 @@ public class Parser {
         return code.subList(equalsIndex + 1, endIndex);
     }
 
-    private static int findMatchingParenthesis(List<Token> code, int startIndex) {
-        int count = 1;
-        for (int i = startIndex + 1; i < code.size(); i++) {
-            if (code.get(i).getType().equals(TokenType.PARENTHESIS) && code.get(i).getValue().equals("(")) {
-                count++;
-            } else if (code.get(i).getType().equals(TokenType.PARENTHESIS) && code.get(i).getValue().equals(")")) {
-                count--;
-                if (count == 0) {
-                    return i;
-                }
-            }
-        }
-
-        throw new RuntimeException("Unmatched parenthesis");
-    }
+//    private static int findMatchingParenthesis(List<Token> code, int startIndex) {
+//        int count = 1;
+//        for (int i = startIndex + 1; i < code.size(); i++) {
+//            if (code.get(i).getType().equals(TokenType.PARENTHESIS) && code.get(i).getValue().equals("(")) {
+//                count++;
+//            } else if (code.get(i).getType().equals(TokenType.PARENTHESIS) && code.get(i).getValue().equals(")")) {
+//                count--;
+//                if (count == 0) {
+//                    return i;
+//                }
+//            }
+//        }
+//
+//        throw new RuntimeException("Unmatched parenthesis");
+//    }
 }
